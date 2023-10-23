73469ff8770fec6d5646a357408cc3b367659454
Nightwindsss <1454059196@qq.com>
2023-10-24 01:20:18 +0800

    菜品相关操作

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -1,16 +1,20 @@
 package com.sky.controller.admin;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.entity.Dish;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
+import com.sky.vo.DishVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.annotations.Delete;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin/dish")
@@ -24,6 +28,48 @@ public class DishController {
     public Result save(@RequestBody DishDTO dishDto){
         log.info("菜品保存:{}",dishDto);
         dishservice.save(dishDto);
+        return Result.success();
+    }
+    /*分页查询菜品*/
+    @GetMapping("/page")
+    @ApiOperation("菜品分页查询")
+    public Result<PageResult> page(DishPageQueryDTO dishPageQueryDTO){
+        PageResult pageResult = dishservice.pageQuery(dishPageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+    /*删除菜品*/
+    @DeleteMapping
+    @ApiOperation("删除菜品")
+    public Result Delete(@RequestParam List<Long> ids){
+        log.info("菜品删除：{}",ids);
+        dishservice.deleteBatch(ids);
+        return Result.success();
+    }
+
+     /*查询菜品*/
+    @GetMapping("/{id}")
+    @ApiOperation("查询菜品信息")
+    public Result<DishVO> getById(@PathVariable Long id){
+        log.info("查询菜品信息：{}",id);
+        DishVO dishVO = dishservice.getByIdwithFlavor(id);
+        return Result.success(dishVO);
+    }
+
+    @PutMapping
+    @ApiOperation("更新菜品信息")
+    /*修改菜品*/
+    public Result update(@RequestBody DishDTO dishDTO){
+        log.info("更新菜品信息：{}",dishDTO);
+        dishservice.update(dishDTO);
+        return Result.success();
+    }
+
+    @ApiOperation("启用禁用分类")
+    @PostMapping("status/{status}")
+    public Result startorstop(@PathVariable Integer status,Long id){
+        log.info("分类启用或者禁用");
+        dishservice.startorstop(status,id);
         return Result.success();
     }
 }
--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,7 +1,23 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.entity.Dish;
+import com.sky.result.PageResult;
+import com.sky.vo.DishVO;
+
+import java.util.List;
 
 public interface DishService {
     public void save(DishDTO dishDTO);
+
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+    void deleteBatch(List<Long> ids);
+
+    DishVO getByIdwithFlavor(Long id);
+
+    void update(DishDTO dishDTO);
+
+    void startorstop(Integer status, Long id);
 }
