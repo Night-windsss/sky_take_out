c7e9e2eddbe534af666e2a5b55ea50e88785647a
Nightwindsss <1454059196@qq.com>
2023-10-30 17:23:47 +0800

    定时完成相关操作（Spring Task）

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -6,6 +6,9 @@ import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Mapper
 public interface OrderMapper {
     void insert(Orders orders);
@@ -19,4 +22,7 @@ public interface OrderMapper {
     Integer countStatus(Integer toBeConfirmed);
     @Select("select * from orders where number = #{orderNumber}")
     Orders getByNumber(String orderNumber);
+
+    @Select("select * from orders where status = #{status} and order_time < #{ordertime}")
+    List<Orders> getByStatusAndOrderTimeLT(Integer status, LocalDateTime ordertime);
 }
